fee22048260da4c609fcc7a991232e237385ac81
Nikhilk70 <nikhilksiva70@gmail.com>
2025-05-03 19:53:45 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+function selectSize(btn) {
+    const allButtons = document.querySelectorAll('.size-btn');
+    allButtons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+    
+    // Set selected size to hidden input (useful for forms)
+    document.getElementById('selectedSize').value = btn.textContent;
+  }
+  
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
