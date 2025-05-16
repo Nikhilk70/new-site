d68c296cace7a6fcf8bb0ef13cb4fd8638e0d181
Nikhilk70 <nikhilksiva70@gmail.com>
2025-05-16 21:10:24 +0530

    Updated grid layout and image size 3

--- js/custom.js
+++ js/custom.js
@@ -42,13 +42,3 @@ function selectSize(btn) {
     // Set selected size to hidden input (useful for forms)
     document.getElementById('selectedSize').value = btn.textContent;
   }
-  
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
